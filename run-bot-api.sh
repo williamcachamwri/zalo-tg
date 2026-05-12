bf597c1c9dcfbd66ac06f8c2fc8378393ddaefc9
William Cachamwri <ToshikoSteinhaus01544@hotmail.com>
2026-05-12 22:27:23 +0700

    Update run-bot-api.sh

--- run-bot-api.sh
+++ run-bot-api.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mkdir -p /Users/wica/lq/zalo-tg/data/bot-api
 exec telegram-bot-api \
-  --api-id=20424880 \
-  --api-hash=a2d3c16c240caa293d3581a1e78cdd08 \
+  --api-id= \
+  --api-hash= \
   --local \
   --dir=/Users/wica/lq/zalo-tg/data/bot-api \
   --temp-dir=/tmp \
